68d0fab3036c2228a47f962376ebb31e791a7bee
Ceren <ceren1481@hotmail.com>
2026-01-23 02:10:02 +0300

    add tests for retrieving TV show episodes by title, season, and episode

--- src/test/java/TestsByIdOrTitle.java
+++ src/test/java/TestsByIdOrTitle.java
@@ -7,13 +7,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestsByIdOrTitle {
     String url = "http://www.omdbapi.com";
+    String apiKey = "3cad8349";
     String errorMessageWhen401 = "No API key provided.";
 
     @Test
     public void shouldSearchByTitle() {
         Response responseByTitle = RestAssured.given()
                 .queryParam("t", "Batman")
-                .queryParam("apikey", "3cad8349")
+                .queryParam("apikey", apiKey)
                 .get(url)
                 .then()
                 .statusCode(200)
@@ -28,7 +29,7 @@ public class TestsByIdOrTitle {
         Response responseByImdbIDAndYear = RestAssured.given()
                 .queryParam("i", "tt0096895")
                 .queryParam("y", "1989")
-                .queryParam("apikey", "3cad8349")
+                .queryParam("apikey", apiKey)
                 .get(url)
                 .then()
                 .statusCode(200)
@@ -41,7 +42,7 @@ public class TestsByIdOrTitle {
     public void shouldSearchByImdbID() {
         Response responseByImdbID = RestAssured.given()
                 .queryParam("i", "tt0096895")
-                .queryParam("apikey", "3cad8349")
+                .queryParam("apikey", apiKey)
                 .get(url)
                 .then()
                 .statusCode(200)
@@ -67,7 +68,7 @@ public class TestsByIdOrTitle {
     public void shouldRatingsValueGreaterThanFifty() {
         Response responseByTitle = RestAssured.given()
                 .queryParam("t", "Batman")
-                .queryParam("apikey", "3cad8349")
+                .queryParam("apikey", apiKey)
                 .get(url)
                 .then()
                 .statusCode(200)
@@ -81,7 +82,7 @@ public class TestsByIdOrTitle {
     public void shouldTypeIsMovie() {
         Response response = RestAssured.given()
                 .queryParam("t", "Shrek")
-                .queryParam("apikey", "3cad8349")
+                .queryParam("apikey", apiKey)
                 .get(url)
                 .then()
                 .statusCode(200)
@@ -94,7 +95,7 @@ public class TestsByIdOrTitle {
     public void shouldPosterIsURL() {
         Response response = RestAssured.given()
                 .queryParam("t", "Shrek")
-                .queryParam("apikey", "3cad8349")
+                .queryParam("apikey", apiKey)
                 .get(url)
                 .then()
                 .statusCode(200)
@@ -107,7 +108,7 @@ public class TestsByIdOrTitle {
     public void shouldLanguageIsEnglish() {
         Response response = RestAssured.given()
                 .queryParam("t", "Shrek")
-                .queryParam("apikey", "3cad8349")
+                .queryParam("apikey", apiKey)
                 .get(url)
                 .then()
                 .statusCode(200)
@@ -120,7 +121,7 @@ public class TestsByIdOrTitle {
     public void shouldMovieHasOscar() {
         Response response = RestAssured.given()
                 .queryParam("t", "Shrek")
-                .queryParam("apikey", "3cad8349")
+                .queryParam("apikey", apiKey)
                 .get(url)
                 .then()
                 .statusCode(200)
@@ -134,13 +135,96 @@ public class TestsByIdOrTitle {
     public void shouldBoxOfficeGreaterThan850Million() {
         Response response = RestAssured.given()
                 .queryParam("t", "Avengers: Endgame")
-                .queryParam("apikey", "3cad8349")
+                .queryParam("apikey", apiKey)
                 .get(url)
                 .then()
                 .statusCode(200)
                 .extract().response();
         String boxOffice = response.getBody().jsonPath().getString("BoxOffice");
-        int boxOfficeValue = Integer.parseInt(boxOffice.replace("$", "").replace(",", ""));
+        int boxOfficeValue = Integer.parseInt(boxOffice.replace("$", "").replace(",",
+                ""));
         assertThat(boxOfficeValue, Matchers.greaterThan(850_000_000));
+    }
+
+    @Test
+    public void shouldGetTvShowEpisodeByTitleSeasonAndEpisode() {
+        Response response = RestAssured.given()
+                .queryParam("t", "Game of Thrones")
+                .queryParam("Season", "1")
+                .queryParam("Episode", "6")
+                .queryParam("apikey", apiKey)
+                .get(url)
+                .then()
+                .statusCode(200)
+                .extract().response();
+        String episodeTitle = response.getBody().jsonPath().getString("Title");
+        assertThat(episodeTitle, Matchers.equalToIgnoringCase("A Golden Crown"));
+        String season = response.getBody().jsonPath().getString("Season");
+        assertThat(season, Matchers.equalTo("1"));
+        String episode = response.getBody().jsonPath().getString("Episode");
+        assertThat(episode, Matchers.equalTo("6"));
+        String seriesID = response.getBody().jsonPath().getString("seriesID");
+        assertThat(seriesID, Matchers.equalTo("tt0944947"));
+    }
+
+    @Test
+    public void shouldSeriesIdSameForDifferentEpisodes() {
+        Response response = RestAssured.given()
+                .queryParam("t", "Game of Thrones")
+                .queryParam("Season", "1")
+                .queryParam("Episode", "6")
+                .queryParam("apikey", apiKey)
+                .get(url)
+                .then()
+                .statusCode(200)
+                .extract().response();
+        String seriesID = response.getBody().jsonPath().getString("seriesID");
+        assertThat(seriesID, Matchers.equalTo("tt0944947"));
+
+        Response response2 = RestAssured.given()
+                .queryParam("t", "Game of Thrones")
+                .queryParam("Season", "1")
+                .queryParam("Episode", "7")
+                .queryParam("apikey", apiKey)
+                .get(url)
+                .then()
+                .statusCode(200)
+                .extract().response();
+        String seriesID2 = response2.getBody().jsonPath().getString("seriesID");
+        assertThat(seriesID, Matchers.equalTo(seriesID2));
+    }
+
+    @Test
+    public void shouldEpisodeInfoMatchWhenSearchByOnlySeasonAndSearchBySeasonAndEpisode() {
+        Response response = RestAssured.given()
+                .queryParam("i", "tt0944947")
+                .queryParam("Season", "2")
+                .queryParam("apikey", apiKey)
+                .get(url)
+                .then()
+                .statusCode(200)
+                .extract().response();
+        String episode1TitleFromSeasonResponse = response.getBody().jsonPath().getString("Episodes[0].Title");
+        String imdbID1FromSeasonResponse = response.getBody().jsonPath().getString("Episodes[0].imdbID");
+        String releaseDateFromSeasonResponse = response.getBody().jsonPath().getString("Episodes[0].Released");
+        String releaseDateYearFromSeasonResponse = releaseDateFromSeasonResponse.split("-")[0]; //"2012-04-01"
+
+        Response response2 = RestAssured.given()
+                .queryParam("t", "Game of Thrones")
+                .queryParam("Season", "2")
+                .queryParam("Episode", "1")
+                .queryParam("apikey", apiKey)
+                .get(url)
+                .then()
+                .statusCode(200)
+                .extract().response();
+        String episode1TitleFromEpisodeResponse = response2.getBody().jsonPath().getString("Title");
+        String imdbIDFromEpisodeResponse = response2.getBody().jsonPath().getString("imdbID");
+        String releaseDateFromEpisodeResponse = response2.getBody().jsonPath().getString("Released");
+        String releaseDateYearFromEpisodeResponse = releaseDateFromEpisodeResponse.split(" ")[2]; //"01 Apr 2012"
+
+        assertThat(episode1TitleFromSeasonResponse, Matchers.equalToIgnoringCase(episode1TitleFromEpisodeResponse));
+        assertThat(imdbID1FromSeasonResponse, Matchers.equalTo(imdbIDFromEpisodeResponse));
+        assertThat(releaseDateYearFromSeasonResponse, Matchers.equalTo(releaseDateYearFromEpisodeResponse));
     }
 }
