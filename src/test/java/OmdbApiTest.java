2c494b6111303dd724047d4eebfdb02f0798a19c
Ceren <ceren1481@hotmail.com>
2026-01-22 18:07:52 +0300

    Added tests for OMDB API responses and refactor URL usage

--- src/test/java/OmdbApiTest.java
+++ src/test/java/OmdbApiTest.java
@@ -7,6 +7,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class OmdbApiTest {
 
+    String url = "http://www.omdbapi.com";
     String errorMessageWhen401 = "No API key provided.";
 
     @Test
@@ -14,12 +15,13 @@ public class OmdbApiTest {
         Response responseByTitle = RestAssured.given()
                 .queryParam("t", "Batman")
                 .queryParam("apikey", "3cad8349")
-                .get("http://www.omdbapi.com")
+                .get(url)
                 .then()
                 .statusCode(200)
                 .extract().response();
         responseByTitle.prettyPrint();
-        assertThat(responseByTitle.getBody().jsonPath().getString("Title"), Matchers.equalToIgnoringCase("Batman"));
+        assertThat(responseByTitle.getBody().jsonPath().getString("Title"),
+                Matchers.equalToIgnoringCase("Batman"));
     }
 
     @Test
@@ -28,11 +30,12 @@ public class OmdbApiTest {
                 .queryParam("i", "tt0096895")
                 .queryParam("y", "1989")
                 .queryParam("apikey", "3cad8349")
-                .get("http://www.omdbapi.com")
+                .get(url)
                 .then()
                 .statusCode(200)
                 .extract().response();
-        assertThat(responseByImdbIDAndYear.getBody().jsonPath().getString("Title"), Matchers.equalToIgnoringCase("Batman"));
+        assertThat(responseByImdbIDAndYear.getBody().jsonPath().getString("Title"),
+                Matchers.equalToIgnoringCase("Batman"));
     }
 
     @Test
@@ -40,24 +43,105 @@ public class OmdbApiTest {
         Response responseByImdbID = RestAssured.given()
                 .queryParam("i", "tt0096895")
                 .queryParam("apikey", "3cad8349")
-                .get("http://www.omdbapi.com")
+                .get(url)
                 .then()
                 .statusCode(200)
                 .extract().response();
-        assertThat(responseByImdbID.getBody().jsonPath().getString("Title"), Matchers.equalToIgnoringCase("Batman"));
+        assertThat(responseByImdbID.getBody().jsonPath().getString("Title"),
+                Matchers.equalToIgnoringCase("Batman"));
     }
 
     @Test
     public void shouldNotGetResponseWithoutApiKey() {
         Response responseWhen401 = RestAssured.given()
                 .queryParam("t", "Batman")
-                .get("http://www.omdbapi.com")
+                .get(url)
                 .then()
                 .statusCode(401)
                 .extract()
                 .response();
-        assertThat(responseWhen401.getBody().jsonPath().getString("Error"), Matchers.equalToIgnoringCase(errorMessageWhen401));
+        assertThat(responseWhen401.getBody().jsonPath().getString("Error"),
+                Matchers.equalToIgnoringCase(errorMessageWhen401));
     }
 
+    @Test
+    public void shouldRatingsValueGreaterThanFifty() {
+        Response responseByTitle = RestAssured.given()
+                .queryParam("t", "Batman")
+                .queryParam("apikey", "3cad8349")
+                .get(url)
+                .then()
+                .statusCode(200)
+                .extract().response();
+        String ratingsValue = responseByTitle.getBody().jsonPath().getString("Ratings[1].Value");
+        int rating = Integer.parseInt(ratingsValue.split("%")[0]);
+        assertThat(rating, Matchers.greaterThan(50));
+    }
 
+    @Test
+    public void shouldTypeIsMovie() {
+        Response response = RestAssured.given()
+                .queryParam("t", "Shrek")
+                .queryParam("apikey", "3cad8349")
+                .get(url)
+                .then()
+                .statusCode(200)
+                .extract().response();
+        String type = response.getBody().jsonPath().getString("Type");
+        assertThat(type, Matchers.equalToIgnoringCase("movie"));
+    }
+
+    @Test
+    public void shouldPosterIsURL() {
+        Response response = RestAssured.given()
+                .queryParam("t", "Shrek")
+                .queryParam("apikey", "3cad8349")
+                .get(url)
+                .then()
+                .statusCode(200)
+                .extract().response();
+        String posterUrl = response.getBody().jsonPath().getString("Poster");
+        assertThat(posterUrl, Matchers.startsWith("https://"));
+    }
+
+    @Test
+    public void shouldLanguageIsEnglish() {
+        Response response = RestAssured.given()
+                .queryParam("t", "Shrek")
+                .queryParam("apikey", "3cad8349")
+                .get(url)
+                .then()
+                .statusCode(200)
+                .extract().response();
+        String language = response.getBody().jsonPath().getString("Language");
+        assertThat(language, Matchers.containsString("English"));
+    }
+
+    @Test
+    public void shouldMovieHasOscar() {
+        Response response = RestAssured.given()
+                .queryParam("t", "Shrek")
+                .queryParam("apikey", "3cad8349")
+                .get(url)
+                .then()
+                .statusCode(200)
+                .extract().response();
+        String awards = response.getBody().jsonPath().getString("Awards");
+        boolean hasTheMovieOscar = awards.split("Oscar")[0].contains("Won");
+        assertThat(hasTheMovieOscar, Matchers.is(true));
+    }
+
+    @Test
+    public void shouldBoxOfficeGreaterThan850Million() {
+        Response response = RestAssured.given()
+                .queryParam("t", "Avengers: Endgame")
+                .queryParam("apikey", "3cad8349")
+                .get(url)
+                .then()
+                .statusCode(200)
+                .extract().response();
+        String boxOffice = response.getBody().jsonPath().getString("BoxOffice");
+        int boxOfficeValue = Integer.parseInt(boxOffice.replace("$", "").replace(",", ""));
+        assertThat(boxOfficeValue, Matchers.greaterThan(850_000_000));
+    }
 }
